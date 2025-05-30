be20517bd02559169340997262be5be0fc4f7c84
will <willneimann@gmail.com>
2025-05-30 12:54:00 +0900

    CL-0010: 홈, 상세페이지 SEO최적화 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 // page.tsx
-"use client";
+import { Metadata } from 'next';
+import HomeClient from './HomeClient';
 
-import PageHeader from '@/components/common/PageHeader';
-import { ListingGrid } from '@/components/listings/ListingGrid';
+export const metadata: Metadata = {
+  title: '채널링크 | 유튜브 채널 안전거래 마켓',
+  description:
+    '채널링크는 유튜브 채널을 안전하고 신뢰성 있게 거래할 수 있는 국내 대표 마켓입니다. 다양한 카테고리의 유튜브 채널을 빠르게 등록하고, 검증된 판매자와 안전하게 거래하세요. 채널 매매, 양도, 인수까지 한 번에!'
+};
 
 export default function Home() {
-  return (
-    <>
-      <PageHeader title="채널 목록" />
-      <ListingGrid />
-    </>
-  );
+  return <HomeClient />;
 }
